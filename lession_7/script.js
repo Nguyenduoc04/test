eebf19b164efbdb53d9f04dcf8df7172ba419aee
Nguyenduoc04 <duocnguyenphuoc159@gmail.com>
2025-11-12 03:27:38 +0700

    update CV

---
+++ lession_7/script.js
@@ -0,0 +1,23 @@
+//var, let, const
+
+var hoten = 'Nguyen Phuoc Duoc';
+var tuoi = 30;
+var ketHon = false;
+var hoTen = 'CodeGym';
+console.log(hoten);
+console.log(tuoi);
+console.log(ketHon);
+// Cú pháp hiện đại ngày nay (let, const)
+let hoVaTen = 'Nguyen Phuoc Duoc';
+hoVaTen = 'Nguyen Phuoc Dat';
+console.log(hoVaTen);
+//let hoVaTen = 'CodeGym';
+
+const PI = 3.1415926579;
+console.log(PI);
+//const OPENAI_KEY = '';
+//const GEMINI_KEY = '';
+//const APP_OPEN_WEATHER_KEY = '';
+
+const firstName = 'Bob';
+console.log(firstName);
